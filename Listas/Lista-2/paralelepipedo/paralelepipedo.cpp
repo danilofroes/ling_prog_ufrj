1564aee15badbbd15bf451c4d2154a50616e2c00
Danilo Fróes <danilofroes.20241@poli.ufrj.br>
2025-09-14 17:12:09 -0300

    FIX: Corrigindo lógica e adicionando mensagem para numeros menor q zero

--- Listas/Lista-2/paralelepipedo/paralelepipedo.cpp
+++ Listas/Lista-2/paralelepipedo/paralelepipedo.cpp
@@ -1,4 +1,7 @@
 #include "paralelepipedo.h"
+#include <iostream>
+
+using namespace std;
 
 Paralelepipedo::Paralelepipedo(double inX, double inY, double inZ) {
     setX(inX);
@@ -7,24 +10,33 @@ Paralelepipedo::Paralelepipedo(double inX, double inY, double inZ) {
 }
 
 void Paralelepipedo::setX(double inX) {
-    if (inX < 0)
+    if (inX <= 0) {
         x = 1;
+        cout << "\nDimensao X eh negativa ou zero, assumindo valor 1 por padrao.";
+    }
 
-    x = inX;
+    else
+        x = inX;
 }
 
 void Paralelepipedo::setY(double inY) {
-    if (inY < 0)
+    if (inY <= 0) {
         y = 1;
+        cout << "\nDimensao Y eh negativa ou zero, assumindo valor 1 por padrao.";
+    }
 
-    y = inY;
+    else
+        y = inY;
 }
 
 void Paralelepipedo::setZ(double inZ) {
-    if (inZ < 0)
+    if (inZ <= 0) {
         z = 1;
+        cout << "\nDimensao Z eh negativa ou zero, assumindo valor 1 por padrao.";
+    }
 
-    z = inZ;
+    else
+        z = inZ;
 }
 
 double Paralelepipedo::computeVolume() { return x * y * z; }
