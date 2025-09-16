78a2c806c5aa4f12d4b15ffec5e9d3f59ab66852
Danilo Fróes <danilofroes.20241@poli.ufrj.br>
2025-09-15 23:41:44 -0300

    FIX: Corrigindo colunas no codigo em Cobol

--- exercicios-livres/Primeiras Linguagens de Programação/manipulacao_dados.cbl
+++ exercicios-livres/Primeiras Linguagens de Programação/manipulacao_dados.cbl
@@ -1,29 +1,33 @@
-IDENTIFICATION DIVISION.
-PROGRAM-ID. RELATORIO_FUNCIONARIO
-AUTHOR. Danilo Froes
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. RELATORIO-FUNCIONARIO.
+       AUTHOR. Danilo Froes.
 
-DATA DIVISION.
-WORKING-STORAGE SECTION.
+       ENVIRONMENT DIVISION.
+       CONFIGURATION SECTION.
+       SPECIAL-NAMES.
+           DECIMAL-POINT IS COMMA.
 
-01 WS-FUNCIONARIO.
-      05 FUNC-ID      PIC 9(4)    VALUE 1812.
-      05 FUNC-NOME    PIC X(40)   VALUE "Danilo Froes".
-      05 FUNC-CARGO   PIC X(40)   VALUE "Eng. de Eletronica e Computacao".
-      05 FUNC-SALARIO PIC 9(5)V99 VALUE 12903.00.
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
 
-01 WS-SALARIO-FORMATADO PIC R$ ZZZ.ZZ9,99.
+       01 WS-FUNCIONARIO.
+          05 FUNC-ID       PIC 9(04)  VALUE 1812.
+          05 FUNC-NOME     PIC X(40)  VALUE "Danilo Froes".
+          05 FUNC-CARGO PIC X(40) VALUE "Eng. de Eletronica e Comp.".
+          05 FUNC-SALARIO  PIC 9(5)V99 VALUE 12903.00.
+       01 WS-SALARIO-FORMATADO.
+          05 FILLER             PIC X(3) VALUE "R$ ".
+          05 WS-SALARIO-EDITADO PIC ZZZ.ZZ9,99.
 
-PROCEDURE DIVISION.
-PROGRAMA-PRINCIPAL.
-      PERFORM EXIBE-RELATORIO.
-      
-      STOP RUN.
+       PROCEDURE DIVISION.
+           PERFORM EXIBE-RELATORIO.
+           STOP RUN.
 
-EXIBE-RELATORIO.
-      DISPLAY "--------- RELATORIO DE DADOS DO FUNCIONARIO ---------".
-      DISPLAY "ID do Funcionario..: " FUNC-ID.
-      DISPLAY "Nome...............: " FUNC-NOME.
-      DISPLAY "Cargo..............: " FUNC-CARGO.
-      MOVE FUNC-SALARIO TO WS-SALARIO-FORMATADO.
-      DISPLAY "Salario............: " WS-SALARIO-FORMATADO.
-      DISPLAY "-----------------------------------------------------".
+       EXIBE-RELATORIO.
+           DISPLAY "-------- RELATORIO DE DADOS DO FUNCIONARIO -------".
+           DISPLAY "ID do Funcionario..: " FUNC-ID.
+           DISPLAY "Nome...............: " FUNC-NOME.
+           DISPLAY "Cargo..............: " FUNC-CARGO.
+           MOVE FUNC-SALARIO TO WS-SALARIO-EDITADO.
+           DISPLAY "Salario............: " WS-SALARIO-FORMATADO.
+           DISPLAY "--------------------------------------------------".
