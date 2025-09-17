64f27f570a4a415ef55aad482fa29f36cd9c3636
Danilo Fróes <danilofroes.20241@poli.ufrj.br>
2025-09-17 00:32:45 -0300

    FIX: Adicionando o EXIT. no fim da função no código em COBOL

--- exercicios-livres/Primeiras Linguagens de Programação/manipulacao_dados.cbl
+++ exercicios-livres/Primeiras Linguagens de Programação/manipulacao_dados.cbl
@@ -9,12 +9,12 @@
 
        DATA DIVISION.
        WORKING-STORAGE SECTION.
-
+           
        01 WS-FUNCIONARIO.
           05 FUNC-ID       PIC 9(04)  VALUE 1812.
           05 FUNC-NOME     PIC X(40)  VALUE "Danilo Froes".
-          05 FUNC-CARGO PIC X(40) VALUE "Eng. de Eletronica e Comp.".
-          05 FUNC-SALARIO  PIC 9(5)V99 VALUE 12903.00.
+          05 FUNC-CARGO PIC X(40) VALUE "Eng. de Eletronica e Comput.".
+          05 FUNC-SALARIO  PIC 9(5)V99 VALUE 12903,00.
        01 WS-SALARIO-FORMATADO.
           05 FILLER             PIC X(3) VALUE "R$ ".
           05 WS-SALARIO-EDITADO PIC ZZZ.ZZ9,99.
@@ -31,3 +31,4 @@
            MOVE FUNC-SALARIO TO WS-SALARIO-EDITADO.
            DISPLAY "Salario............: " WS-SALARIO-FORMATADO.
            DISPLAY "--------------------------------------------------".
+           EXIT.
