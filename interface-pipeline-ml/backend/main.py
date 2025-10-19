d5f2b96054bcca7ac9bfa942bf3d6a2f4464a1e2
Danilo Fróes <danilofroes.20241@poli.ufrj.br>
2025-10-18 21:14:49 -0300

    ADD: Arquivos do trabalho de interface para construção de pipeline em ml

---
+++ interface-pipeline-ml/backend/main.py
@@ -0,0 +1,10 @@
+'''
+    Programa para automação e experimentação em pipelines de problemas de classificação de Machine Learning
+
+    Autor: Danilo Davi Gomes Fróes
+    Matéria: Linguagens de Programação [EEL670]
+    Professor: Miguel Elias Mitre Campista
+'''
+
+def __main__():
+    print("hello")
---
+++ interface-pipeline-ml/gerenciador/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+#define OK 0
+
+int main() {
+    
+
+    return OK;
+}
