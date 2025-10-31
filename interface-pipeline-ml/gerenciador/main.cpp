ca41991bb5788cd4c9d6c699f95a145c15455e2e
Danilo Fróes <danilofroes.20241@poli.ufrj.br>
2025-10-31 05:30:04 -0300

    ADD: Main do gerenciador

--- interface-pipeline-ml/gerenciador/main.cpp
+++ interface-pipeline-ml/gerenciador/main.cpp
@@ -1,12 +1,55 @@
 #include <iostream>
 #include <cstdlib>
+#include <string>
+
+#include "menu.h"
 
 using namespace std;
 
 #define OK 0
 
+extern string pathDataset;
+extern string colunaTarget;
+extern enum MetodoDivisao { HOLDOUT, VALIDACAO_CRUZADA } metodo_divisao;
+
 int main() {
-    
+    unsigned short int opcao;
+
+    do {
+        exibirMenuPrincipal();
+        cin >> opcao;
+
+        switch (opcao) {
+            case 1:
+                do {
+                    exibirMenuDados();
+                    cin >> opcao;
+
+                    switch (opcao) {
+                        case 1:
+                            cin >> pathDataset;
+                            break;
+                        case 2:
+                            cout << "Funcionalidade de visualizar dataset ainda nao implementada." << endl;
+                            break;
+                        case 0:
+                            cout << "Retornando ao menu principal..." << endl;
+                            break;
+                        default:
+                            cout << "Opcao invalida. Tente novamente." << endl;
+                            break;
+                    }
+                } while (opcao != 0);
+                break;
+
+            case 0:
+                cout << "Saindo do programa..." << endl;
+                break;
+            default:
+                cout << "Opcao invalida. Tente novamente." << endl;
+                break;
+        }
+    } while (opcao != 0);
 
     return OK;
 }
