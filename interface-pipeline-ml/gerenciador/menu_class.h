5abb11ab0a5a49ff303d01621be3c8a8a0d4df38
Danilo Fróes <danilofroes.20241@poli.ufrj.br>
2025-10-31 05:29:28 -0300

    ADD: Arquivos para menu do trabalho final com classe

---
+++ interface-pipeline-ml/gerenciador/menu_class.h
@@ -0,0 +1,16 @@
+#ifndef MENU_H
+#define MENU_H
+
+#include <string>
+
+class MenuPrincipal {
+    public:
+        static std::string pathDataset;
+        static bool pathDatasetAdicionado;
+        static std::string colunaTarget;
+        static bool colunaTargetAdicionada;
+        
+
+};
+
+#endif
